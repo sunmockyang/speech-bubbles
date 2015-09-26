a943072336d41bfa9c2e06c278a536c65f3a2254
Sunmock Yang <sunmock@live.ca>
2015-09-26 18:48:20 -0400

    Combined TailLocation and getTargetIntersectionPoint function

--- SpeechBubble.js
+++ SpeechBubble.js
@@ -3,9 +3,14 @@
 
 function SpeechBubble(context) {
 	this.context = context;
-	this.panelBounds = new Bounds(10, 10, 200, 100);
+	this.panelBounds = new Bounds(100, 100, 200, 100);
 	this.target = {x: 100, y: 300};
 }
+
+SpeechBubble.TOP_SIDE = "SPEECH_BUBBLE_TOP";
+SpeechBubble.BOTTOM_SIDE = "SPEECH_BUBBLE_BOTTOM";
+SpeechBubble.LEFT_SIDE = "SPEECH_BUBBLE_LEFT";
+SpeechBubble.RIGHT_SIDE = "SPEECH_BUBBLE_RIGHT";
 
 SpeechBubble.prototype.draw = function() {
 	this.context.strokeStyle = "#000";
@@ -21,18 +26,19 @@ SpeechBubble.prototype.draw = function() {
 	this.context.strokeStyle = "#F00";
 	this.context.beginPath();
 	this.context.moveTo(this.panelBounds.getCenter().x, this.panelBounds.getCenter().y);
-	this.context.lineTo(this.getTargetIntersectionPoint().x, this.getTargetIntersectionPoint().y);
+	this.context.lineTo(this.getTailLocation().x, this.getTailLocation().y);
 	this.context.stroke();
 	this.context.closePath();
 
 	this.context.fillStyle = "#0F0";
 	this.context.fillRect(this.target.x, this.target.y, 5, 5);
 	// this.context.fillRect(this.panelBounds.getCenter().x, this.panelBounds.getCenter().y, 5, 5);
-	// this.context.fillRect(this.getTargetIntersectionPoint().x, this.getTargetIntersectionPoint().y, 5, 5);
+	// this.context.fillRect(this.getTailLocation().x, this.getTailLocation().y, 5, 5);
 };
 
-SpeechBubble.prototype.getTargetIntersectionPoint = function() {
+SpeechBubble.prototype.getTailLocation = function() {
 	var x = 0, y = 0;
+	var side = "";
 
 	var boundsCenter = this.panelBounds.getCenter();
 	var relativeTargetX = this.target.x - boundsCenter.x;
@@ -45,36 +51,19 @@ SpeechBubble.prototype.getTargetIntersectionPoint = function() {
 	{
 		y = this.panelBounds.height/2 * Math.sign(relativeTargetY);
 		x = relativeTargetX * (y / relativeTargetY);
+		side = (Math.sign(relativeTargetY) > 0) ? SpeechBubble.BOTTOM_SIDE : SpeechBubble.TOP_SIDE;
 	}
 	else
 	{
 		x = this.panelBounds.width/2 * Math.sign(relativeTargetX);
 		y = relativeTargetY * (x / relativeTargetX);
+		side = (Math.sign(relativeTargetX) > 0) ? SpeechBubble.RIGHT_SIDE : SpeechBubble.LEFT_SIDE;
 	}
 
 	x += boundsCenter.x;
 	y += boundsCenter.y;
 
-	return {x: x, y: y};
-};
-
-SpeechBubble.prototype.TailLocation = function() {
-	var side = "";
-	
-	var boundsCenter = this.panelBounds.getCenter();
-	var relativeTargetX = this.target.x - boundsCenter.x;
-	var relativeTargetY = this.target.y - boundsCenter.y;
-	var targetAspectRatio = relativeTargetX / relativeTargetY;
-	var boundsAspectRatio = this.panelBounds.width / this.panelBounds.height;
-
-	if (Math.abs(targetAspectRatio) < Math.abs(boundsAspectRatio)){
-		side = (Math.sign(relativeTargetY) > 0) ? "bottom" : "top";
-	}
-	else {
-		side = (Math.sign(relativeTargetX) > 0) ? "right" : "left";
-	}
-
-	return side;
+	return {x: x, y: y, side:side};
 };
 
 function Bounds(top, left, width, height){
