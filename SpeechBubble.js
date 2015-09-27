66efc0b37eb7c3046862f5c3c8b8362d1cff6f15
Sunmock Yang <sunmock@live.ca>
2015-09-27 03:39:00 -0400

    Draw the tail and clamp the tail location
    - the tail base will not overflow outside the rectangle

--- SpeechBubble.js
+++ SpeechBubble.js
@@ -5,6 +5,7 @@ function SpeechBubble(context) {
 	this.context = context;
 	this.panelBounds = new Bounds(100, 100, 200, 100);
 	this.target = {x: 100, y: 300};
+	this.tailBaseWidth = 10;
 }
 
 SpeechBubble.TOP_SIDE = "SPEECH_BUBBLE_TOP";
@@ -12,17 +13,48 @@ SpeechBubble.BOTTOM_SIDE = "SPEECH_BUBBLE_BOTTOM";
 SpeechBubble.LEFT_SIDE = "SPEECH_BUBBLE_LEFT";
 SpeechBubble.RIGHT_SIDE = "SPEECH_BUBBLE_RIGHT";
 
+SpeechBubble.clamp = function(val, min, max) {
+	return Math.min(Math.max(val, min), max);
+};
+
 SpeechBubble.prototype.draw = function() {
+	var tailLocation = this.getTailLocation();
+
 	this.context.strokeStyle = "#000";
 	this.context.beginPath();
 	this.context.moveTo(this.panelBounds.left, this.panelBounds.top);
-	this.context.lineTo(this.panelBounds.left + this.panelBounds.width, this.panelBounds.top);
-	this.context.lineTo(this.panelBounds.left + this.panelBounds.width, this.panelBounds.top + this.panelBounds.height);
-	this.context.lineTo(this.panelBounds.left, this.panelBounds.top + this.panelBounds.height);
-	this.context.lineTo(this.panelBounds.left, this.panelBounds.top);
+	this.drawPanelWall(SpeechBubble.TOP_SIDE, tailLocation, this.panelBounds.left + this.panelBounds.width, this.panelBounds.top);
+	this.drawPanelWall(SpeechBubble.RIGHT_SIDE, tailLocation, this.panelBounds.left + this.panelBounds.width, this.panelBounds.top + this.panelBounds.height);
+	this.drawPanelWall(SpeechBubble.BOTTOM_SIDE, tailLocation, this.panelBounds.left, this.panelBounds.top + this.panelBounds.height);
+	this.drawPanelWall(SpeechBubble.LEFT_SIDE, tailLocation, this.panelBounds.left, this.panelBounds.top);
+
 	this.context.stroke();
 	this.context.closePath();
+};
 
+SpeechBubble.prototype.drawPanelWall = function(panelSide, tailLocation, toX, toY) {
+	if (panelSide == tailLocation.side) {
+		var baseWidth = this.tailBaseWidth;
+		if (panelSide == SpeechBubble.TOP_SIDE || panelSide == SpeechBubble.RIGHT_SIDE) {
+			baseWidth *= -1;
+		}
+
+		if (panelSide == SpeechBubble.TOP_SIDE || panelSide == SpeechBubble.BOTTOM_SIDE) {
+			this.context.lineTo(tailLocation.x + baseWidth, toY);
+			this.context.lineTo(this.target.x, this.target.y);
+			this.context.lineTo(tailLocation.x - baseWidth, toY);
+		}
+		else {
+			this.context.lineTo(toX, tailLocation.y + baseWidth);
+			this.context.lineTo(this.target.x, this.target.y);
+			this.context.lineTo(toX, tailLocation.y - baseWidth);
+		}
+	}
+
+	this.context.lineTo(toX, toY);
+};
+
+SpeechBubble.prototype.drawDebug = function() {
 	this.context.strokeStyle = "#F00";
 	this.context.beginPath();
 	this.context.moveTo(this.panelBounds.getCenter().x, this.panelBounds.getCenter().y);
@@ -32,8 +64,8 @@ SpeechBubble.prototype.draw = function() {
 
 	this.context.fillStyle = "#0F0";
 	this.context.fillRect(this.target.x, this.target.y, 5, 5);
-	// this.context.fillRect(this.panelBounds.getCenter().x, this.panelBounds.getCenter().y, 5, 5);
-	// this.context.fillRect(this.getTailLocation().x, this.getTailLocation().y, 5, 5);
+	this.context.fillRect(this.panelBounds.getCenter().x, this.panelBounds.getCenter().y, 5, 5);
+	this.context.fillRect(this.getTailLocation().x, this.getTailLocation().y, 5, 5);
 };
 
 SpeechBubble.prototype.getTailLocation = function() {
@@ -59,6 +91,9 @@ SpeechBubble.prototype.getTailLocation = function() {
 		y = relativeTargetY * (x / relativeTargetX);
 		side = (Math.sign(relativeTargetX) > 0) ? SpeechBubble.RIGHT_SIDE : SpeechBubble.LEFT_SIDE;
 	}
+
+	x = SpeechBubble.clamp(x, this.tailBaseWidth - this.panelBounds.width/2, this.panelBounds.width/2 - this.tailBaseWidth);
+	y = SpeechBubble.clamp(y, this.tailBaseWidth - this.panelBounds.height/2, this.panelBounds.height/2 - this.tailBaseWidth);
 
 	x += boundsCenter.x;
 	y += boundsCenter.y;
